98e427d9df6bc78cb476850721b8c6b55987ad95
Mirovinkin <137717625+Mirovinkin@users.noreply.github.com>
2023-10-25 20:29:05 +0500

    Update MyMatrix2.0.cpp

--- MyMatrix2.0/MyMatrix2.0.cpp
+++ MyMatrix2.0/MyMatrix2.0.cpp
@@ -155,6 +155,41 @@ public:
         }
         return matrixRes;
     }
+T MatrixMax() {
+        T max = 0;
+        for (int i = 0; i < _rows; i++)
+        {
+            for (int j = 0; j < _cols; j++)
+            {
+                if (max(_matrix[i][j], _matrix[i][j + 1])) {
+                    max = _matrix[i][j];
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    T MatrixMin() {
+        T min = 0;
+        for (int i = 0; i < _rows; i++)
+        {
+            for (int j = 0; j < _cols; j++)
+            {
+                if (min(_matrix[i][j], _matrix[i][j + 1])) {
+                    max = _matrix[i][j];
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+
+        return min;
+    }
 };
 int main()
 {
